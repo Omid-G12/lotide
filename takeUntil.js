afd6c7c09513a4196e3437ad4389cc15b3d890a3
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-08-30 16:22:19 +0000

    refactored using modules

--- takeUntil.js
+++ takeUntil.js
@@ -1,26 +1,5 @@
-const eqArrays = function(arrOne, arrTwo) {
-  let result = true;
-  if (arrOne.join() === arrTwo.join()) {
-    for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] === arrTwo[i]) {;
-        continue;
-      } else {
-        result = false;
-      }
-    }
-  } else {
-    result = false;
-  }
-  return result;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 //New code: 
 
@@ -41,7 +20,9 @@ const takeUntil = function(array, callback) {
   return output;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+module.exports = takeUntil;
+
+/* const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
 
@@ -53,4 +34,4 @@ console.log(results2);
 
 assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
 console.log('---');
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood']);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood']); */
