585446a03f4a159c2438d55832e3eac6348af389
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-08-30 16:19:22 +0000

    refactored with modules

--- map.js
+++ map.js
@@ -1,26 +1,5 @@
-const eqArrays = function(arrOne, arrTwo) {
-  let result = true;
-  if (arrOne.join() === arrTwo.join()) {
-    for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] === arrTwo[i]) {;
-        continue;
-      } else {
-        result = false;
-      }
-    }
-  } else {
-    result = false;
-  }
-  return result;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const map = function(array, callback) {
   
@@ -33,9 +12,11 @@ const map = function(array, callback) {
   return output;
 }
 
-const words = ["ground", "control", "to", "major", "tom"];
+module.exports = map;
+
+/* const words = ["ground", "control", "to", "major", "tom"];
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
 
-assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']); */
