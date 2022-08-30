c107249d5d31e7637d80ce52bdf4b91af40bf2de
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-08-30 16:00:24 +0000

    refactored with modules

--- countOnly.js
+++ countOnly.js
@@ -1,12 +1,6 @@
 //Copy your assertEqual function into this new file.
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 //New code
 
@@ -27,7 +21,8 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-const firstNames = [
+module.exports = countOnly;
+/* const firstNames = [
   "Karl",
   "Salima",
   "Agouhanna",
@@ -44,4 +39,4 @@ const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": t
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Agouhanna"], undefined); */
