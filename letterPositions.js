2e6666cf86b88e21dd5beb9fdc51253df76042f8
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-08-30 16:15:58 +0000

    refactored with modules

--- letterPositions.js
+++ letterPositions.js
@@ -1,27 +1,6 @@
 //Previous code:
-const eqArrays = function(arrOne, arrTwo) {
-  let result = true;
-  if (arrOne.join() === arrTwo.join()) {
-    for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] === arrTwo[i]) {;
-        continue;
-      } else {
-        result = false;
-      }
-    }
-  } else {
-    result = false;
-  }
-  return result;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 //New Code:
 const letterPositions = function(sentence) {
@@ -38,6 +17,8 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-console.log(letterPositions("lighthouse in the house"));
+module.exports = letterPositions;
 
-assertArraysEqual(letterPositions("hello").e, [1]);
+/* console.log(letterPositions("lighthouse in the house"));
+
+assertArraysEqual(letterPositions("hello").e, [1]); */
